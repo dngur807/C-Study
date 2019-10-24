50d6920319ec802f2eb8e65faa555263c8a792eb
whjeong <dngur807@naver.com>
2019-10-25 01:06:11 +0900

    tcp 패킷 수신 처리

---
+++ GameCodiLab/FreeNet/FreeNet/CUserToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreeNet
+{
+    public class CUserToken
+    {
+        // 바이트를 패킷 형식으로 해석해주는 해석기.
+        CMessageResolver message_resolver;
+
+        void on_receive(byte[] buffer , int offset , int transferred)
+        {
+            this.message_resolver.on_receive(buffer, offset, transferred);
+        }
+    }
+}
